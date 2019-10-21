802706e2a896634d358c2d5040df883dfcfb077b
vinuruban <vinu_06@hotmail.com>
2019-10-21 16:48:44 +0100

    initial commit at home

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Todos from "./components/Todos";
 
 class App extends Component {
-
   state = {
-    todos: [ //array - []
-      { //objects - {}
+    todos: [
+      //array - []
+      {
+        //objects - {}
         id: 1,
-        title: 'Wake up',
+        title: "Wake up",
         completed: false
       },
       {
         id: 2,
-        title: 'Go gym',
+        title: "Go gym",
         completed: true
       },
       {
         id: 3,
-        title: 'Go sleep',
+        title: "Go sleep",
         completed: false
       }
     ]
-  }
+  };
 
-  markComplete = (e) => {
+  markComplete = e => {
     //this deletes the record, based on an index that we pass through, and return the new array
     this.setState({
-      todos: [ //array - []
-      { //objects - {}
-        id: 1,
-        title: 'Wake up',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Dont go gym',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Eat sweets',
-        completed: false
-      }
-    ]
-        //filter does not mutate but rather creates a new array, and is a preferred method for modifying arrays in JavaScript. This particular method is testing an index vs. all the indices in the array, and returning all but the one that is passed through.
-    })
-}
+      todos: [
+        //array - []
+        {
+          //objects - {}
+          id: 1,
+          title: "Wake up",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Dont go gym",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Eat sweets",
+          completed: false
+        }
+      ]
+      //filter does not mutate but rather creates a new array, and is a preferred method for modifying arrays in JavaScript. This particular method is testing an index vs. all the indices in the array, and returning all but the one that is passed through.
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>App.js</h1>
-        <Todos todosParam={this.state.todos} cTodos={this.changeTodos} markComplete1={this.markComplete}/>
+        <Todos
+          todosParam={this.state.todos}
+          markComplete1={this.markComplete}
+        />
       </div>
     );
   }
