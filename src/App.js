73e62a538904dc3e126f3aac53db1da7037a5032
Shantharoopan <vinu.ruban@netbuilder.com>
2019-10-22 12:19:19 +0100

    complete! - Api calls learnt - Used 'axios' to get, post and delete data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import "./App.css";
+import Header from "./components/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import About from "./components/pages/About";
+// import uuid from 'uuid';
+import axios from 'axios';
 
 class App extends Component {
   state = {
-    todos: [
-      //array - []
-      {
-        //objects - {}
-        id: 1,
-        title: "Wake up",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Go gym",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Go sleep",
-        completed: false
-      }
-    ]
+    todos: []
   };
+
+  //install 'npm i axios' for api calls
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(result => 
+      this.setState({
+        todos: result.data
+      }))
+  }
 
   //the func below takes the id when the onChange=...bing() is triggered in the ToDoItem.js comp, then finds the item in the state using the id, and then changes/switches/toggles the completed boolean
   markComplete = id => {
@@ -42,39 +38,66 @@ class App extends Component {
 
   // [...] <- this copies everything that's already there. This func filters out any 'listin' that has the id which is not equal to the id that passed in as the parameter
   deleteData = id => {
-    //this deletes the record, based on an index that we pass through, and return the new array
-    this.setState({
+    // //this deletes the record, based on an index that we pass through, and return the new array
+    // this.setState({
+    //   todos: [...this.state.todos.filter(listin => listin.id !== id)]
+    //   //filter does not mutate but rather creates a new array, and is a preferred method for modifying arrays in JavaScript. This particular method is testing an index vs. all the indices in the array, and returning all but the one that is passed through.
+    // });
+
+    axios.delete('https://jsonplaceholder.typicode.com/todos/' + id)
+    .then(result => this.setState({
       todos: [...this.state.todos.filter(listin => listin.id !== id)]
       //filter does not mutate but rather creates a new array, and is a preferred method for modifying arrays in JavaScript. This particular method is testing an index vs. all the indices in the array, and returning all but the one that is passed through.
-    });
+    }))
   };
 
   addData = title => {
-    const newToDo = {
-      id: 4, // this can be anything
-      title: title, //the second title is the title that's passed in
-      complete: false
-    };
 
-    this.setState({
-      todos: [...this.state.todos, newToDo]
-      // the above copies the current this.state.todos, and also add the newToDo (see above)
-    });
+    // const newToDo = {
+    //   id: uuid.v4(), // this randomly generates a number - install 'npm i uuid'
+    //   title: title, //the second title is the title that's passed in
+    //   complete: false
+    // };
+    //
+    // this.setState({
+    //   todos: [...this.state.todos, newToDo]
+    //   // the above copies the current this.state.todos, and also adds the newToDo (see above)
+    // });
+
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title: title,
+      completed: false
+    })
+      .then(result => this.setState({
+      todos: [...this.state.todos, result.data]
+      // the above copies the current this.state.todos, and also adds the result.data (see above)
+    }))
+    //the above posts data in the server first, and then updates the UI (our front end)    
   };
 
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <h1 className="title">To-do List</h1>
-          <AddTodo addData1={this.addData} />
-          <Todos
-            todosParam={this.state.todos}
-            markComplete1={this.markComplete}
-            deleteData1={this.deleteData}
-          />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addData1={this.addData} />
+                <Todos
+                  todosParam={this.state.todos}
+                  markComplete1={this.markComplete}
+                  deleteData1={this.deleteData}
+                />
+              </React.Fragment>  
+            )} />
+
+            <Route path="/about" component={About} />
+
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+function About() {
+    return (
+        <React.Fragment>
+            <h2>About</h2>
+            <p>This is the about page. Install 'npm i react-router-dom' prior to this.</p>
+        </React.Fragment>
+    )
+  }
+
+export default About;
